ce8612b8e0f87f8edd32b7b58dc367a38e6e9e28
Bruuuuu22 <00001103367432sp@al.educacao.sp.gov.br>
2024-10-21 08:08:30 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'pessoal',
+    'time do coração?',
+    'corinthians'
+)
+
+criaCartao(
+    'pessoal',
+    'série favorita?',
+    'stranger things'
+)
+
+criaCartao(
+    'recomendação',
+    'livro favorito?',
+    'harry potter'
+)
+
+criaCartao(
+    'recomendacão',
+    'matéria que mais gosta?',
+    'física'
+)
